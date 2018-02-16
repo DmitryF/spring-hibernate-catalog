12f9bee440d0c382a6afb9cb7bce19ed1eee9555
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-16 22:16:40 +0300

    Rename package controllers

--- src/main/java/com/dzmitryf/catalog/controllers/CatalogController.java
+++ src/main/java/com/dzmitryf/catalog/controllers/CatalogController.java
@@ -1,4 +1,4 @@
-package com.dzmitryf.catalog.controller;
+package com.dzmitryf.catalog.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
