7e6a8f6b55864b67572f617c894178a9ee194fed
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-16 20:41:43 +0300

    Comment repository

---
+++ src/main/java/com/dzmitryf/catalog/repositories/CommentRepository.java
@@ -0,0 +1,19 @@
+package com.dzmitryf.catalog.repositories;
+
+import com.dzmitryf.catalog.model.User;
+import com.dzmitryf.catalog.model.comment.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    /**
+     * Retrieves comments by user
+     * @param user
+     * @return the comments with given user
+     */
+    List<Comment> findAllByUser(User user);
+}
