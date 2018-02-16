382b66d622a1ab36aeb7f9ac2ef3e38d5222a8d8
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-17 00:55:05 +0300

    Add logger

--- src/main/java/com/dzmitryf/catalog/model/base/BaseEntityListener.java
+++ src/main/java/com/dzmitryf/catalog/model/base/BaseEntityListener.java
@@ -1,15 +1,23 @@
 package com.dzmitryf.catalog.model.base;
 
+import com.dzmitryf.catalog.services.impl.BookServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.util.Date;
 
 public class BaseEntityListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseEntityListener.class);
+
     @PrePersist
     public void prePersistBaseEntity(BaseEntity entity){
         if (entity != null && entity.getCreatingDate() == null) {
-            entity.setCreatingDate(new Date());
+            Date creatinDate = new Date();
+            entity.setCreatingDate(creatinDate);
+            LOGGER.info("New entity creating date: {}", creatinDate);
         }
         updateEditingDate(entity);
     }
@@ -21,7 +29,9 @@ public class BaseEntityListener {
 
     private void updateEditingDate(BaseEntity entity){
         if (entity != null) {
-            entity.setEditingDate(new Date());
+            Date editingDate = new Date();
+            entity.setEditingDate(editingDate);
+            LOGGER.info("New entity editing date: {}", editingDate);
         }
     }
 }
