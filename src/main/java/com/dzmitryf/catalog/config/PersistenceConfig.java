c0fec909458bb58068985435f03f2ac2098e733f
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-16 19:41:32 +0300

    Replace @Environment to @Value

--- src/main/java/com/dzmitryf/catalog/config/PersistenceConfig.java
+++ src/main/java/com/dzmitryf/catalog/config/PersistenceConfig.java
@@ -1,47 +1,87 @@
 package com.dzmitryf.catalog.config;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
+/**
+ * Application hibernate configuration
+ */
+
 @Configuration
 @PropertySource({ "classpath:hibernate-config.properties" })
-@ComponentScan("com.dzmitryf.catalog")
-@EnableJpaRepositories(value = "com.dzmitryf.catalog.dao",
+@ComponentScan({"com.dzmitryf.catalog.config",
+                "com.dzmitryf.catalog.services",
+                "com.dzmitryf.catalog.repositories"
+                })
+@EnableJpaRepositories(value = "com.dzmitryf.catalog.repositories",
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef = "transactionManager")
 @EnableTransactionManagement
 public class PersistenceConfig {
 
-    @Autowired
-    private Environment environment;
+    private static final String PACKAGE_MODEL = "com.dzmitryf.catalog.model";
 
+    private static final String HIBERNATE_DIALECT = "hibernate.dialect";
+    private static final String HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private static final String HIBERNATE_BYTECODE_OPTIMIZER = "hibernate.bytecode.use_reflection_optimizer";
+    private static final String SHOW_SQL = "show_sql";
+
+    @Value("${hibernate.connection.driver_class}")
+    private String dataSourceDriver;
+
+    @Value("${hibernate.connection.url}")
+    private String dataSourceUrl;
+
+    @Value("${hibernate.connection.username}")
+    private String dataSourceUsername;
+
+    @Value("${hibernate.connection.password}")
+    private String dataSourcePassword;
+
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hibernateHbm2ddl;
+
+    @Value("${hibernate.bytecode.use_reflection_optimizer}")
+    private String hibrnateBytecodeOptimizer;
+
+    @Value("${show_sql}")
+    private String showSql;
+
+    /**
+     * Get {@link LocalContainerEntityManagerFactoryBean} for Jpa repositories
+     * @return entity manager factory
+     */
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
         LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
         entityManager.setDataSource(dataSource());
-        entityManager.setPackagesToScan(new String[] { "com.dzmitryf.catalog.model" });
+        entityManager.setPackagesToScan(PACKAGE_MODEL);
         entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        entityManager.setJpaProperties(additionalProperties());
+        entityManager.setJpaProperties(getHibernateProperties());
 
         return entityManager;
     }
 
+    /**
+     * Get {@link JpaTransactionManager} for Jpa repositories
+     * @return transaction manager
+     */
     @Bean
     public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
@@ -49,23 +89,31 @@ public class PersistenceConfig {
         return transactionManager;
     }
 
-    private Properties additionalProperties(){
+    /**
+     * Get hibernate {@link Properties} for Jpa for entity manager
+     * @return hibernate properties for entity manager
+     */
+    private Properties getHibernateProperties(){
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
-        hibernateProperties.setProperty("hibernate.bytecode.use_reflection_optimizer", environment.getProperty("hibernate.bytecode.use_reflection_optimizer"));
-        hibernateProperties.setProperty("show_sql", environment.getProperty("show_sql"));
+        hibernateProperties.setProperty(HIBERNATE_DIALECT, hibernateDialect);
+        hibernateProperties.setProperty(HIBERNATE_HBM2DDL_AUTO, hibernateHbm2ddl);
+        hibernateProperties.setProperty(HIBERNATE_BYTECODE_OPTIMIZER, hibrnateBytecodeOptimizer);
+        hibernateProperties.setProperty(SHOW_SQL, showSql);
 
         return hibernateProperties;
     }
 
+    /**
+     * Get {@link DataSource} for {@link LocalContainerEntityManagerFactoryBean}
+     * @return data source for entity manager
+     */
     @Bean
     public DataSource dataSource(){
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(environment.getProperty("hibernate.connection.driver_class"));
-        dataSource.setUrl(environment.getProperty("hibernate.connection.url"));
-        dataSource.setUsername(environment.getProperty("hibernate.connection.username"));
-        dataSource.setPassword(environment.getProperty("hibernate.connection.password"));
+        dataSource.setDriverClassName(dataSourceDriver);
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
 
         return dataSource;
     }
