4ac61d5e4947b9e36e60fab8c152b74033c18d34
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-16 19:42:29 +0300

    Refactoring repositories

---
+++ src/main/java/com/dzmitryf/catalog/repositories/BookRepository.java
@@ -0,0 +1,26 @@
+package com.dzmitryf.catalog.repositories;
+
+import com.dzmitryf.catalog.model.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    /**
+     * Retrieve book by name
+     * @param name book name
+     * @return the book with given name or {@literal null} if none found
+     */
+    Book findBookByName(String name);
+
+    /**
+     * Retrivies books with max count pages
+     * @return the books that sorted by descending
+     */
+    @Query(value = "SELECT * FROM hbschema.books ORDER BY count_pages DESC", nativeQuery = true)
+    List<Book> findBooksByCountPagesDesc();
+}
--- src/main/java/com/dzmitryf/catalog/repositories/UserRepository.java
+++ src/main/java/com/dzmitryf/catalog/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-package com.dzmitryf.catalog.dao;
+package com.dzmitryf.catalog.repositories;
 
 import com.dzmitryf.catalog.model.User;
 import com.dzmitryf.catalog.model.book.Book;
@@ -9,16 +9,23 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User save(User user);
-
+    /**
+     * Retrieve user by first name
+     * @param firstName
+     * @return the user with given first name or {@literal null} if none found
+     */
     User findUserByFirstName(String firstName);
 
+    /**
+     * Retrivies users by count books
+     * @return the users who sorted by max count of books
+     */
     @Query(value = "SELECT * FROM hbschema.users\n" +
             "LEFT JOIN (\n" +
             "SELECT COUNT(user_id), user_id FROM hbschema.user_book \n" +
             "GROUP BY user_id \n" +
             "ORDER BY count DESC) u ON id = u.user_id", nativeQuery = true)
-    List<User> getUsersByCountBooksDesc();
+    List<User> findUsersByCountBooksDesc();
 }
