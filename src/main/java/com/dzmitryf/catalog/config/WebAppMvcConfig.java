4ad5433d5c3eefa0917951337726395dd3773d26
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-18 21:51:14 +0300

    Add Json converters
    Add Json converters

--- src/main/java/com/dzmitryf/catalog/config/WebAppMvcConfig.java
+++ src/main/java/com/dzmitryf/catalog/config/WebAppMvcConfig.java
@@ -2,8 +2,16 @@ package com.dzmitryf.catalog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @EnableWebMvc
 @Configuration
@@ -12,5 +20,17 @@ public class WebAppMvcConfig extends WebMvcConfigurerAdapter {
 
     public WebAppMvcConfig() {
         super();
+    }
+
+    /**
+     * Set up Json converters
+     * @param converters list
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder = new Jackson2ObjectMapperBuilder();
+        converters.add(new MappingJackson2HttpMessageConverter(jackson2ObjectMapperBuilder.build()));
+        Jaxb2RootElementHttpMessageConverter jaxb2RootElementHttpMessageConverter = new Jaxb2RootElementHttpMessageConverter();
+        converters.add(jaxb2RootElementHttpMessageConverter);
     }
 }
