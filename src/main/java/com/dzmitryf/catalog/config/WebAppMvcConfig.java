d94b89b2dd26e65d945710b68ab17185ac560d54
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-21 15:58:35 +0300

    Remove imports

--- src/main/java/com/dzmitryf/catalog/config/WebAppMvcConfig.java
+++ src/main/java/com/dzmitryf/catalog/config/WebAppMvcConfig.java
@@ -1,6 +1,5 @@
 package com.dzmitryf.catalog.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,12 +7,8 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 import java.util.List;
 
