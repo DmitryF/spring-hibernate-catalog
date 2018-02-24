206e0f2571b2dd978af4756cae2eab19efe2d48d
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-25 00:43:24 +0300

    CatalogController catches of exceptions

--- src/main/java/com/dzmitryf/catalog/controllers/CatalogController.java
+++ src/main/java/com/dzmitryf/catalog/controllers/CatalogController.java
@@ -2,10 +2,13 @@ package com.dzmitryf.catalog.controllers;
 
 import com.dzmitryf.catalog.model.user.User;
 import com.dzmitryf.catalog.services.UserService;
+import com.dzmitryf.catalog.services.impl.ApiServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -28,21 +31,47 @@ public class CatalogController {
      * @return the user with given username
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public @ResponseBody User login(@RequestParam(value = "username", required = false) String username) {
-        LOGGER.info("Request user login complete: {}", username);
-        User user = userService.getUserByUsername(username);
-        LOGGER.info("Response has user: {}", user);
-        return user;
+    public @ResponseBody User login(@RequestParam(value = "username", required = false) String username, Locale locale) throws ApiServiceException {
+        try {
+            LOGGER.info(messageSource.getMessage("catalog.controller.login.complete", new Object[]{username}, locale));
+            User user = userService.getUserByUsername(username, locale);
+            LOGGER.info(messageSource.getMessage("catalog.controller.login.response.user", new Object[]{user}, locale));
+            return user;
+        }
+        catch (ApiServiceException e) {
+            throw e;
+        } catch (Exception e){
+            LOGGER.error(messageSource.getMessage("catalog.controller.error.login", new Object[]{username}, locale), e);
+            throw new ApiServiceException(messageSource.getMessage("application.error.internal", null, locale));
+        }
     }
 
     /**
      * Access denied redirect
      * @param locale current user {@link Locale}
-     * @return locale message
+     * @return {@link ErrorResponse} with status 403
      */
     @RequestMapping(value = "/403", method = RequestMethod.GET)
-    public @ResponseBody String accessDenied(Locale locale) {
-        LOGGER.info("Request error 403. Access denied");
-        return messageSource.getMessage("security.access_denied", null, locale);
+    public @ResponseBody ResponseEntity<ErrorResponse> accessDenied(Locale locale) throws ApiServiceException{
+        try {
+            LOGGER.info(messageSource.getMessage("security.access_denied", null, locale));
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.value(),
+                    messageSource.getMessage("security.access_denied", null, locale));
+            return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.FORBIDDEN);
+        } catch (Exception e){
+            LOGGER.error(messageSource.getMessage("catalog.controller.error.access.denied", null, locale));
+            throw new ApiServiceException(messageSource.getMessage("application.error.internal", null, locale));
+        }
+    }
+
+    /**
+     * Catalog controller exception handler
+     * @param apiException
+     * @return {@link ErrorResponse} with given status from {@link ApiServiceException}
+     */
+    @ExceptionHandler(ApiServiceException.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(ApiServiceException apiException){
+        ErrorResponse errorResponse = new ErrorResponse(apiException.getStatusCode().value(), apiException.getMessage());
+        return new ResponseEntity<ErrorResponse>(errorResponse, apiException.getStatusCode());
     }
 }
