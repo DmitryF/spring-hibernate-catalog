595bb9790a1ca4cb049262efb0963d3a74446535
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-25 13:01:55 +0300

    Exceptions for create user role

--- src/main/java/com/dzmitryf/catalog/services/impl/UserRoleServiceImpl.java
+++ src/main/java/com/dzmitryf/catalog/services/impl/UserRoleServiceImpl.java
@@ -1,17 +1,19 @@
 package com.dzmitryf.catalog.services.impl;
 
 import com.dzmitryf.catalog.model.base.SecurityRole;
+import com.dzmitryf.catalog.model.user.User;
 import com.dzmitryf.catalog.model.user.UserRole;
 import com.dzmitryf.catalog.repositories.UserRoleRepository;
 import com.dzmitryf.catalog.services.UserRoleService;
+import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
+import javax.persistence.*;
 import javax.transaction.Transactional;
 import java.util.Locale;
 
@@ -19,6 +21,9 @@ import java.util.Locale;
 public class UserRoleServiceImpl implements UserRoleService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRoleServiceImpl.class);
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Autowired
     private UserRoleRepository userRoleRepository;
@@ -34,24 +39,50 @@ public class UserRoleServiceImpl implements UserRoleService {
         return userRole;
     }
 
-    @Transactional
+    /**
+     * Create a given user role. Use the returned instance for further operations as the save operation might have changed the
+     * user role instance completely.
+     * @param userRole role must note be {@literal null}
+     * @param locale
+     * @return the saved user role
+     * @throws Exception
+     * @throws ApiServiceException if user role not entity or already exists
+     */
+    @Transactional(rollbackOn = ApiServiceException.class)
     @Override
-    public UserRole create(UserRole entity, Locale locale) throws Exception {
-        LOGGER.info("Creating a new user role: {}", entity);
-        UserRole userRole = new UserRole();
-        userRole.update(entity);
-        if (!isUserRoleExist(entity)){
-            try {
-                entityManager.persist(userRole);
-                entityManager.flush();
-                LOGGER.info("Created a new user role: {}", userRole);
-            } catch (Exception e) {
-                LOGGER.error("Error while creating a new user role: ", e);
+    public UserRole create(UserRole userRole, Locale locale) throws Exception {
+        LOGGER.info(messageSource.getMessage("user.role.service.create.user.role", new Object[]{userRole}, locale));
+        UserRole createdUserRole = new UserRole();
+        try {
+            createdUserRole.update(userRole);
+            entityManager.persist(createdUserRole);
+            entityManager.flush();
+            LOGGER.info(messageSource.getMessage("user.role.service.created.user.role", new Object[]{createdUserRole}, locale));
+        } catch (EntityExistsException e) {
+            LOGGER.info(messageSource.getMessage("user.role.service.user.role.already.exist",
+                    new Object[]{userRole.getSecurityRole().toString()}, locale));
+            throw new ApiServiceException(messageSource.getMessage("user.role.service.user.role.already.exist",
+                    new Object[]{userRole.getSecurityRole().toString()}, locale),
+                    HttpStatus.PRECONDITION_FAILED);
+        } catch (PersistenceException e) {
+            if (e.getCause() instanceof ConstraintViolationException) {
+                LOGGER.info(messageSource.getMessage("user.role.service.user.role.is.unique",
+                        new Object[]{userRole.getSecurityRole().toString()}, locale));
+                throw new ApiServiceException(messageSource.getMessage("user.role.service.user.role.is.unique",
+                        new Object[]{userRole.getSecurityRole().toString()}, locale),
+                        HttpStatus.PRECONDITION_FAILED);
+            } else {
+                throw e;
             }
-        } else {
-            LOGGER.info("User role already exist: {}", entity);
+        } catch (IllegalArgumentException e){
+            LOGGER.info(messageSource.getMessage("user.role.service.user.role.not.entity", new Object[]{userRole.getId()}, locale));
+            throw new ApiServiceException(messageSource.getMessage("user.service.user.not.entity", new Object[]{userRole.getId()}, locale),
+                    HttpStatus.PRECONDITION_FAILED);
+        } catch (Exception e) {
+            LOGGER.error(messageSource.getMessage("user.role.service.error.create.user.role", null, locale), e);
+            throw e;
         }
-        return userRole;
+        return createdUserRole;
     }
 
     /**
