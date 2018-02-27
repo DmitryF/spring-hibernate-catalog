ba54eaa594b2c1cadd3174f2a3ed68013ba11c09
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-27 17:10:12 +0300

    BookController catches of exceptions

--- src/main/java/com/dzmitryf/catalog/controllers/BookController.java
+++ src/main/java/com/dzmitryf/catalog/controllers/BookController.java
@@ -1,10 +1,14 @@
 package com.dzmitryf.catalog.controllers;
 
 import com.dzmitryf.catalog.model.book.Book;
+import com.dzmitryf.catalog.model.comment.Comment;
 import com.dzmitryf.catalog.services.BookService;
+import com.dzmitryf.catalog.services.impl.ApiServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,30 +21,62 @@ public class BookController {
     private static final Logger LOGGER = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
     private BookService bookService;
 
     /**
      * Get all books
-     * @return
+     * @return books list or {@literal ApiServiceException} if none found
+     * @throws ApiServiceException if books not found
      */
     @RequestMapping(value = "all", method = RequestMethod.GET)
     public @ResponseBody
     List<Book> getAllBooks(Locale locale) throws Exception{
-        LOGGER.info("Request get all books");
-        List<Book> books = bookService.getAllBooks(locale);
-        LOGGER.info("Response has {} books", books.size());
-        return books;
+        try {
+            LOGGER.info(messageSource.getMessage("book.controller.get.book.all", null, locale));
+            List<Book> books = bookService.getAllBooks(locale);
+            LOGGER.info(messageSource.getMessage("book.controller.response.books", new Object[]{books.size()}, locale));
+            return books;
+        } catch (ApiServiceException e){
+            throw e;
+        } catch (Exception e) {
+            LOGGER.error(messageSource.getMessage("book.controller.error.get.book.all", null, locale), e);
+            throw new ApiServiceException(messageSource.getMessage("application.error.internal", null, locale));
+        }
     }
 
     /**
      * Get book by id
-     * @return the book with given id
+     * @param bookId
+     * @param locale
+     * @return the book with given id {@literal ApiServiceException} if none found
+     * @throws ApiServiceException if book not found
      */
     @RequestMapping(value = "{bookId}", method = RequestMethod.GET)
-    public @ResponseBody Book getCmmentById(@PathVariable("bookId") Long bookId, Locale locale) throws Exception{
-        LOGGER.info("Request get book by id: {}", bookId);
-        Book book = bookService.getById(bookId, locale);
-        LOGGER.info("Response has book: {}", book);
-        return book;
+    public @ResponseBody Book getBookById(@PathVariable("bookId") Long bookId, Locale locale) throws Exception{
+        try {
+            LOGGER.info(messageSource.getMessage("book.controller.get.book.by.id", new Object[]{bookId}, locale));
+            Book book = bookService.getById(bookId, locale);
+            LOGGER.info(messageSource.getMessage("book.controller.response.book", new Object[]{book}, locale));
+            return book;
+        } catch (ApiServiceException e){
+            throw e;
+        } catch (Exception e) {
+            LOGGER.error(messageSource.getMessage("book.controller.error.get.book.by.id", new Object[]{bookId}, locale), e);
+            throw new ApiServiceException(messageSource.getMessage("application.error.internal", null, locale));
+        }
+    }
+
+    /**
+     * Book controller exception handler
+     * @param apiException
+     * @return {@link ErrorResponse} with given status from {@link ApiServiceException}
+     */
+    @ExceptionHandler(ApiServiceException.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(ApiServiceException apiException){
+        ErrorResponse errorResponse = new ErrorResponse(apiException.getStatusCode().value(), apiException.getMessage());
+        return new ResponseEntity<ErrorResponse>(errorResponse, apiException.getStatusCode());
     }
 }
