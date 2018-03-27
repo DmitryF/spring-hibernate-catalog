9d199b0cc3c0cd23237e6f2c5b39cbfd7cd148e8
DzmitryF <dzmitryf.developer@gmail.com>
2018-03-27 20:20:42 +0200

    Test get books by genre

--- angular-catalog/src/app/services/book.service.spec.ts
+++ angular-catalog/src/app/services/book.service.spec.ts
@@ -16,6 +16,7 @@ import {
 } from '../services';
 
 import {Book} from '../models/book';
+import {Genre} from '../models/genre';
 
 describe('Service: BookService', () => {
   let service, mockBackend;
@@ -54,6 +55,40 @@ describe('Service: BookService', () => {
       });
 
       const mockReq = httpMock.expectOne(config.books_all_url);
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+
+      mockReq.flush(responseBooks);
+
+      httpMock.verify();
+    })
+  );
+
+  it('should return books by genre', 
+    inject(
+      [BookService, ConfigService, HttpTestingController], 
+      (service, config, httpMock) => {
+
+      let responseBooks = [];
+      const countBooks = 3;
+      let genre: Genre = new Genre();
+      genre.name = 'comedy';
+      for (var indexBook = 0; indexBook < countBooks; indexBook++) {
+        const book: Book = new Book();
+        book.id = indexBook.toString();
+        book.name = "book" + indexBook.toString();
+        book.genre = genre;
+        responseBooks.push(book);      
+      }
+       
+      service.getBooksByGenre('comedy').subscribe(books => {           
+        expect(books).toEqual(responseBooks); 
+        expect(books[0].genre.name).toContain(genre.name);     
+        expect(books.length).toBe(3);
+      });
+
+      const mockReq = httpMock.expectOne(config.books_by_genre_url + genre.name);
 
       expect(mockReq.cancelled).toBeFalsy();
       expect(mockReq.request.responseType).toEqual('json');
